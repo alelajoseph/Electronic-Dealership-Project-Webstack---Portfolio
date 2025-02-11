f3757a8ee04e7d630a3d0724f0c9c176fd8b0614
alelajoseph <109358699+alelajoseph@users.noreply.github.com>
2025-02-11 18:14:29 +0300

    Refactor JavaScript dependencies and custom scripts

---
+++ Ecomm/includes/navbar.php
@@ -0,0 +1,62 @@
+<header class="main-header">
+  <nav class="navbar navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="index.php" class="navbar-brand"><b>Evan</b>Sites</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+          <i class="fa fa-bars"></i>
+        </button>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="aboutus.php">ABOUT US</a></li>
+          <li><a href="contactus.php">CONTACT US</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORY <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <!-- PHP Loop for Category Links -->
+              <li><a href='category.php?category=category_slug'>Category Name</a></li>
+              <!-- End of PHP Loop -->
+            </ul>
+          </li>
+        </ul>
+        <form method="POST" class="navbar-form navbar-left" action="search.php">
+          <div class="input-group">
+              <input type="text" class="form-control" id="navbar-search-input" name="keyword" placeholder="Search for Product" required>
+              <span class="input-group-btn" id="searchBtn" style="display:none;">
+                  <button type="submit" class="btn btn-default btn-flat"><i class="fa fa-search"></i> </button>
+              </span>
+          </div>
+        </form>
+      </div>
+      <!-- /.navbar-collapse -->
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown messages-menu">
+            <!-- Menu toggle button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-shopping-cart"></i>
+              <span class="label label-success cart_count"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="header">You have <span class="cart_count"></span> item(s) in cart</li>
+              <li>
+                <ul class="menu" id="cart_menu">
+                </ul>
+              </li>
+              <li class="footer"><a href="cart_view.php">Go to Cart</a></li>
+            </ul>
+          </li>
+          <!-- User Authentication Links -->
+          <li><a href='login.php'>LOGIN</a></li>
+          <li><a href='signup.php'>SIGNUP</a></li>
+          <!-- End of User Authentication Links -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
